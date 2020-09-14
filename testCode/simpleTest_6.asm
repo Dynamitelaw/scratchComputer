85fc13086d630e1125f2aa0ea6d5a5c5580bde49
Dynamitelaw <noobeater122333444455555@gmail.com>
2020-09-13 21:08:26 -0700

    Added supported for and tested instructions LW,LHU,LBU,SW,SH,and SB to logisim. Still need to implement LH and LB, since memory controller not signExtenging subWords.

--- testCode/simpleTest_6.asm
+++ testCode/simpleTest_6.asm
@@ -8,21 +8,56 @@ begin:
 addi a0, zero, 21	# a0 = 21
 addi a1, zero, 2	# a1 = 2
 
-#Set stack pointer to starting value?
-addi sp, zero, 255
+#Set stack pointer to starting value
+addi sp, zero, 32
 
 #Allocate 8 bytes for the 2 integers
 addi sp, sp, -8
 
 #Store a0 and a1
 sw a0, -4(sp)
-addi sp, sp, 4
-sw a0, 0(sp)
+sw a1, 0(sp)
 
 #Load stored values into a2 and a3
 lw a2, -4(sp)
-addi sp, sp, -4
 lw a3, 0(sp)
 
 #Multiply them, then store result in a0
-mul a0, a2, a3
+mul a0, a2, a3  #a0 = 42
+
+#Clear other registers
+addi a1, zero, 0
+addi a2, zero, 0
+addi a3, zero, 0
+
+####
+# Test sub-word instructions by concatenating four bytes of 1s
+####
+addi t0, zero, 255
+
+sb t0, 0(sp)
+sb t0, 1(sp)
+sb t0, 2(sp)
+sb t0, 3(sp)
+#-1 should now be in 0(sp)
+
+lw a1, 0(sp)
+addi t1, zero, -1
+
+beq a1, t1, a1_make42
+j clearTemps
+
+a1_make42:
+addi a1, zero, 42
+
+clearTemps:
+addi t0, zero, 0
+addi t1, zero, 0
+
+####
+# Test signed/unsigned load instructions
+####
+
+#Replace upper 2 bytes of word with 0s
+sb zero, 2(sp)
+sb zero, 3(sp)
